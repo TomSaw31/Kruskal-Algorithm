76894417a6da3d4258baeb3c48fad3d82e1883d1
TomSaw31 <111185572+TomSaw31@users.noreply.github.com>
2025-03-11 07:36:27 +0100

    fixed names

--- graph.c
+++ graph.c
@@ -2,53 +2,46 @@
 #include <stdlib.h>
 
 
-typedef struct s_Vertex {
-	struct s_Node * n1;
-	struct s_Node * n2;
+typedef struct s_Edge {
+	struct s_Vertex * n1;
+	struct s_Vertex * n2;
 	float weight;
-} Vertex;
-
-typedef struct s_Node {
-	int value;
-} Node;
-
-typedef struct s_VerticesTable {
-	Vertex ** vertices;
-	Vertex ** fast_vertices;
-	unsigned int step;
-} VerticesTable;
+} Edge;
 
 typedef struct tGraph {
 	unsigned int size;
 	unsigned int max_size;
 	unsigned int vertices_count;
-	VerticesTable vertices;
-	Node * nodes;
+	int * vertices;
+	Edge  ** edges;
 }Graph;
+
 /*----- Public Functions -----*/
-typedef void (*GraphVertexMapOperator)(const void* elem, void* user_param);
+typedef void (*GraphEdgeMapOperator)(const void* elem, void* user_param);
 
 Graph graph_create(unsigned int max_size){}
 
-Graph graph_create_node(const int n){}
+Graph graph_create_vertex(const int n){}
 
-Graph graph_add_node(Graph g, const int n){}
+Graph graph_add_vertex(Graph g, const int n){}
 
-Graph graph_add_vertex(Graph g, const int n1, const int n2, const float w){}
+Graph graph_add_edge(Graph g, const int n1, const int n2, const float w){}
 
 void graph_delete(Graph g){}
 
-Graph graph_delete_node(Graph g, const int n){}
+Graph graph_delete_vertex(Graph g, const int n){}
 
-Graph graph_delete_vertex(Graph g, const int n1, const int n2){}
+Graph graph_delete_edge(Graph g, const int n1, const int n2){}
 
-Graph graph_get_node(Graph g, const int n){}
+Graph graph_get_vertex(Graph g, const int n){}
+
+Graph graph_get_vertex(Graph g, const int n1, const int n2){}
 
 float graph_get_edge_weight(Graph g, const int n1, const int n2){}
 
 int * graph_get_neighbors(Graph g, const int n){}
 
-int graph_node_exists(Graph g, const int n1){}
+int graph_vertex_exists(Graph g, const int n1){}
 
 int graph_edge_exists(Graph g, const int n1, const int n2){}
 
@@ -60,7 +53,7 @@ unsigned int graph_vertices_amount(const Graph g){}
 
 int graph_overflow(const Graph g){}
 
-void graph_map(const Graph g, GraphVertexMapOperator f, void* user_param){}
+void graph_map(const Graph g, GraphEdgeMapOperator f, void* user_param){}
 
 /*----- Private Functions -----*/
 
--- graph.h
+++ graph.h
@@ -5,27 +5,29 @@ typedef struct Graph * tGraph;
 
 Graph graph_create(unsigned int max_size);
 
-Graph graph_create_node(const int n);
+Graph graph_create_vertex(const int n);
 
-Graph graph_add_node(Graph g, const int n);
+Graph graph_add_vertex(Graph g, const int n);
 
-Graph graph_add_vertex(Graph g, const int n1, const int n2, const float w);
+Graph graph_add_edge(Graph g, const int n1, const int n2, const float w);
 
 void graph_delete(Graph g);
 
-Graph graph_delete_node(Graph g, const int n);
+Graph graph_delete_vertex(Graph g, const int n);
 
-Graph graph_delete_vertex(Graph g, const int n1, const int n2);
+Graph graph_delete_edge(Graph g, const int n1, const int n2);
 
-Graph graph_get_node(Graph g, const int n);
+Graph graph_get_vertex(Graph g, const int n);
+
+Graph graph_get_vertex(Graph g, const int n1, const int n2){}
 
 float graph_get_edge_weight(Graph g, const int n1, const int n2);
 
 int * graph_get_neighbors(Graph g, const int n);
 
-int graph_node_exists(Graph g, const int n1);
+int graph_vertex_exists(Graph g, const int n1);
 
-int graph_vertex_exists(Graph g, const int n1, const int n2);
+int graph_edge_exists(Graph g, const int n1, const int n2);
 
 int graph_empty(const Graph g);
 
@@ -35,9 +37,9 @@ unsigned int graph_vertices_amount(const Graph g);
 
 int graph_overflow(const Graph g);
 
-typedef void (*GraphVertexMapOperator)(const void* elem, void* user_param);
+typedef void (*GraphEdgeMapOperator)(const void* elem, void* user_param);
 
-void graph_map(const Graph g, GraphVertexMapOperator f, void* user_param);
+void graph_map(const Graph g, GraphEdgeMapOperator f, void* user_param);
 
 #endif
 
